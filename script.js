98aba6e20146bfcc296717625a38e26b27167aa5
Orian Gavish <gavishorian@gmail.com>
2021-11-25 21:40:32 +0200

    Continued working on the project. Still need to add backspace/keyboard functionality/css

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let screen = document.querySelector('.screen');
 let miniScreen = document.querySelector('.miniScreen');
 let screenValue;
-let num1;
-let num2;
+let num1 = 0;
+let num2 = 0;
 let total;
-let test;
+let operation;
+let times = 0;
 
 function add(a, b) {
     return +a + +b;
@@ -31,27 +32,63 @@ function operate(operator, a, b) {
         case '*':
             return (multiply(a, b));
         case '/':
-            return (divide(a, b));
+            return (b == 0) ? "Don't try to break the universe by dividing by 0!" : divide(a, b);
     }
 }
 
 function display(num) {
     screen.textContent += num;
-    screenValue = parseInt(screen.textContent);
+    screenValue = screen.textContent;
+
 }
 
 function operator(operator) {
-    num1 = screen.textContent;
-    test = operator;
-    miniScreen.textContent = num1 + ' ' + operator;
-    screen.textContent = '';
+    if (num1) {
+        num2 = screen.textContent;
+    } else {
+        num1 = screen.textContent;
+    }
+    if (times>0) {
+        equal('special');
+    }
+    operation = operator;
+        miniScreen.textContent = `${num1} ${operation}`;
+        screen.textContent = '';
+        times++;
 }
 
-function equal(operator) {
-    num2 = screen.textContent;
-    total = (operate(test, num1, num2));
-    screen.textContent = total;
-    miniScreen.textContent = num1 + ' ' + test + ' ' + num2 + ' =';
-    num1 = total;
+function equal(type) {
+    if (type == 'regular') {
+        num2 = screenValue;
+        total = (operate(operation, num1, num2));
+        total = (Math.round(total * 1000)) / 1000;
+        screen.textContent = total;
+        (!num1 || !num2) ?
+            miniScreen.textContent = '' : miniScreen.textContent = `${num1} ${operation} ${num2} =`;
+        num1 = total;
+    } else if (type == 'special') {
+        total = (operate(operation, num1, num2));
+        total = (Math.round(total * 1000)) / 1000;
+        num1 = total;
+        console.log('Num1: ' + num1);
+        console.log('Num2: ' + num2);
+        console.log('Total: ' + total);
+        console.log('Operation: ' + operation);
+    }
+}
+
+function clearCalc() {
+    num1 = 0;
+    num2 = '';
+    total = 0;
+    operation = '';
+    times = 0;
+    screen.textContent = '';
+    miniScreen.textContent = '';
+    screenValue = '';
+}
+
+function deleteCalc() {
+    screenValue = screen.textContent.toString().slice(0, -1);
 }
 
